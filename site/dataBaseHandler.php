8129233c47d2509e23e98de1f98f2a17af2aa08b
Florian Mülhauser <florian.mulhauser@arcanite.ch>
2020-09-25 17:30:28 +0200

    add handler for the DB

---
+++ site/dataBaseHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+class DBHandler
+{
+    private $conn;
+    // Database parameters
+    private $servername = "127.0.0.1";
+    private $dbname = "websiteSTI-Project1";
+    private $username = "root";
+    private $password = "root";
+ 
+
+    public function __construct(){
+        // Init connection
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        if (!$this->conn->set_charset("utf8")) { 
+			die("Connection failed: " . $this->conn->connect_error);
+		}
+        if ($this->conn->connect_error) { 
+			die("Connection failed: " . $this->conn->connect_error); 
+		}
+        
+    }
+    
+    public function __destruct() {
+        // Terminate the connection
+        mysqli_close($this->conn);
+    }
+
+
+    public function request($sql) {
+        // Execute some SQL queries
+        return $this->conn->query($sql);
+    }
+
+    
+}
