fbed2eee868d7a70abcc107f55e2a18563ec30d9
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-10-12 21:05:52 +0200

    creating the basic architecture

---
+++ site/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to STI project 1 Homepage</title>
+    <meta name="viewport" charset="utf-8" content="width=device-width">
+</head>
+
+<!--    admin only-->
+<div class="container">
+    <h3>Navigation to other admin only pages</h3>
+    <!--    admin only-->
+    <button type="button" href="register.php" class="btn btn-primary">Register a new user</button>
+
+    <!--    admin only-->
+    <button type="button" href="modifyUser.php" class="btn btn-primary">Modify an existing user</button>
+
+    <!--    admin only-->
+    <button type="button" href="deleteUser.php" class="btn btn-primary">Delete a user</button>
+</div>
+
+<div class="container">
+    <h3>Navigation to other pages</h3>
+
+    <button type="button" href="message.php" class="btn btn-primary">Create a new message</button>
+
+    <button type="button" href="inbox.php" class="btn btn-primary">View your inbox</button>
+
+    <button type="button" href="changePassword.php" class="btn btn-primary">Change your password</button>
+</div>
+</html>
