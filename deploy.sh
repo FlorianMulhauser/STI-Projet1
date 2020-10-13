af13758319ab346d473cd676ff83d84b27698616
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-10-13 10:57:18 +0200

    change script to work with WSL. password for database is now admin i guess

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+docker kill sti_project
+docker rm sti_project
 docker rm $(docker ps -a -q)
-docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+# we put the forward / int front of $PWD/site to make it work on WSL.
+docker run -ti -v "/$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+
 docker exec -u root sti_project service nginx start
 docker exec -u root sti_project service php5-fpm start
