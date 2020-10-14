3fba565a6b85468c34cd5af8eb9dd569c8f1e85c
Robin Cuénoud <robin.cuenoud@heig-vd.ch>
2020-10-14 22:22:17 +0200

    ca avance

--- site/html/inbox.php
+++ site/html/inbox.php
@@ -17,11 +17,17 @@
         echo "<div class='card blue-grey darken-1'>";
             echo "<div class='card-content white-text'>";
                 echo "<span class='card-title'>";
-                    echo $row['title'] . "<br/>";
+                    echo "<b>".$row['title']."</b>";
+                    echo " from <b>".$row['sender']. "</b><br />";
                 echo "</span>";
-                    echo $row['message'] . "<br/>";
+                
+                   // echo $row['message'] . "<br/>";
                 echo '<div class="card-action">';
                     echo "Time: " . $row['time'] . "<br/>";
+                    echo '<a class="btn-floating btn-small waves-effect waves-light green" type="submit" href="inbox.php?reply&id='.$row['id'].'"><i class="material-icons">reply</i></a>';
+                    echo '<a class="btn-floating btn-small waves-effect waves-light green" type="submit" href="inbox.php?details&id='.$row['id'].'" ><i class="material-icons">arrow_downward</i></a>';
+                    echo '<a href="inbox.php?delete&id='.$row['id'].'" class="btn-floating btn-small waves-effect waves-light red" type="submit" ><i class="material-icons">delete_forever</i></a>';
+                    
                 echo '</div>';
             echo "</div>";
         echo "</div>";     
