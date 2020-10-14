904144ec7c3ce4b76cf644775a5f5684befe1dd3
Florian Mülhauser <florian.mulhauser@heig-vd.ch>
2020-10-14 18:27:20 +0200

    change register to radio buttons

--- site/html/register.php
+++ site/html/register.php
@@ -1,26 +1,6 @@
-<?php
-include ("DBHandler.php");
-//require_once 'include/DBHandler.php';
-session_start();
-// Set default timezone
-date_default_timezone_set('UTC');
+<?php include("fragments/DBHandler.php");
 
-$dbh = new DBHandler();
-// connect
-//$dbh->__construct();
-//
-
-//try {
-///**************************************
-// * Create databases and                *
-// * open connections                    *
-// **************************************/
-//
-//// Create (connect to) SQLite database in file
-//$db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
-//// Set errormode to exceptions
-//$db->setAttribute(PDO::ATTR_ERRMODE,
-//    PDO::ERRMODE_EXCEPTION);
+$db = new DBHandler();
 
 $sql = "INSERT INTO USER (USERNAME, PASSWORD, VALIDITY, ROLE)"
     ."\n"."VALUES ('".$_POST["TFusername"].
@@ -29,59 +9,51 @@ $sql = "INSERT INTO USER (USERNAME, PASSWORD, VALIDITY, ROLE)"
     "', '".$_POST["TFrole"]."');";
 
 //$ret = $DB.request($sql);
-$ret = $dbh->execute($sql);
+$db->exec($sql);
 
-    /**************************************
-     * Close db connections                *
-     **************************************/
-
-    // Close file db connection
-    //$db = null;
-
-//$dbh->__destruct();
-
-}
-//catch(PDOException $e) {
-//    // Print PDOException message
-//    echo $e->getMessage();
-//}
 
 ?>
-<?php include 'fragments/header.php';?>
+
+<?php //include 'fragments/header.php';?>
 
 <body>
     <h3> Register a person here</h3>
-    <form action="register.php?uid=<?php echo uniqid()?>" role="form" method="post">
-        <div class="form-group">
-            <label for="TFusername">Username: </label>
-            <input id="TFusername" class="form-control" type="text" placeholder="USERNAME">
-        </div>
+    <div class="container">
 
-        <div class="form-group">
-            <label for="TFpassword">Password: </label>
-            <input id="TFpassword" class="form-control" type="text" placeholder="PASSWORD">
-        </div>
+        <form action="register.php?uid=<?php echo uniqid()?>" role="form" method="post">
+            <div class="form-group">
+                <label for="TFusername">Username: </label>
+                <input id="TFusername" class="form-control" type="text" placeholder="USERNAME">
+            </div>
 
-        <div class="form-group">
+            <div class="form-group">
+                <label for="TFpassword">Password: </label>
+                <input id="TFpassword" class="form-control" type="text" placeholder="PASSWORD">
+            </div>
 
-            <label for="TFvalidity">Choose the validity state:</label>
-            <select id="TFvalidity">
-                <option value="true">Active account</option>
-                <option value="false">Innactive account</option>
-            </select>
-        </div>
 
-        <div class="form-group">
-            <label for="TFrole">Choose the role:</label>
-            <select id="TFrole">
-                <option value="collaborateur">Collaborateur</option>
-                <option value="administrateur">Administrateur</option>
-            </select>
-        </div>
+            <div class="form-group">Choose the activity:
+                <input type="radio" name="TFvalidity"
+                    <?php if (isset($validity) && $validity=="true") echo "checked";?>
+                       value="true">Active
+                <input type="radio" name="TFvalidity"
+                    <?php if (isset($validity) && $validity=="false") echo "checked";?>
+                       value="false">Inactive
+            </div>
 
-        <button class="btn btn-default" type="submit">Register the user</button>
+            <div class="form-group">Choose the role:
+                <input type="radio" name="TFrole"
+                    <?php if (isset($role) && $role=="collaborateur") echo "checked";?>
+                       value="collaborateur">Collaborateur
+                <input type="radio" name="TFrole"
+                    <?php if (isset($role) && $role=="administrateur") echo "checked";?>
+                       value="administrateur">Administrateur
+            </div>
 
-    </form>
+            <button class="btn btn-default" type="submit">Register the user</button>
+
+        </form>
+    </div>
 </body>
-<?php include 'fragments/footer.php';?>
+<?php //include 'fragments/footer.php';?>
 
