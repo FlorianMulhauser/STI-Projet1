499021bde6ed3b805845eb0522cd0e4ddbb6bdda
Robin Cuénoud <robin.cuenoud@heig-vd.ch>
2020-10-15 19:29:03 +0200

    test voir

--- site/html/modifyUser.php
+++ site/html/modifyUser.php
@@ -3,49 +3,53 @@
 
 include("fragments/DBHandler.php");
 
-//include 'fragments/header.php';
 
-$username = $password = $validity = $role = "";
+
+$username = $password = $validity = $role = $id =  "";
 $username_err = $password_err = $validity_err = $role_err = "";
 
 $db = new DBHandler();
 
-if($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET"){
-
-    // Check if username is empty
-    if(empty(trim($_POST["TFusername"]))){
+if(empty($_GET['TFusername'])) {
         $username_err = "Please enter username.";
     } else{
-        $username = trim($_POST["TFusername"]);
+        $username = $_GET["TFusername"];
+        
+        $sql = "SELECT * FROM user WHERE username = '".$username."';";
+        $result = $db->request($sql)->fetch();
+        // assign existing values of the user
+        $id = $result['id'];
+        $password = $result['password'];
+        $validity = $result['validity'];
+        $role = $result['role'];
     }
+        
+    
+    
+if($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET" ){
+    
+    // Check if username is empty
+    
     // Check if password is empty
-    if(empty(trim($_POST["TFpassword"]))){
+    if(empty(trim(isset($_POST["TFpassword"])))){
         $password_err = "Please enter password.";
     } else{
         $password = trim($_POST["TFpassword"]);
     }
     // Check if validity is empty
-    if(empty(trim($_POST["TFvalidity"]))){
+    if(empty(trim(isset($_POST["TFvalidity"])))){
         $validity_err = "Please give the validity.";
     } else{
         $validity = trim($_POST["TFvalidity"]);
     }
     // Check if role is empty
-    if(empty(trim($_POST["TFrole"]))){
+    if(empty(trim(isset($_POST["TFrole"])))){
         $role_err = "Please enter the role.";
     } else{
         $role = trim($_POST["TFrole"]);
     }
 
-    if(!empty($username)){
-        $sql = "SELECT * FROM user WHERE username = '".$username."';";
-        $result = $db->request($sql)->fetch();
-
-        // assign existing values of the user
-        $password = $result['password'];
-        $validity = $result['validity'];
-        $role = $result['role'];
-    }
+   
 
     if(!empty($username) && !empty($password) && !empty($validity) && !empty($role)){
         if(empty($username_err) && empty($validity_err) && empty($password_err) && empty($role_err)){
@@ -54,21 +58,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET"){
             $db->exec($sqlUpdate);
             echo "Updated !";
             exit;
-        } else {
-
-            echo "an error ocurred";
-        }
+        } 
     }
 
 }
 
 ?>
-
-<h1> Hello there admin ! </h1>
+<?php //include("fragments/header.php"); ?>
 <body>
-<h3> Modify a user here</h3>
-<div class="container">
 
+
+<div class="container">
+<?php if(empty($username)) { 
+    echo ' <h4> Hello there admin ! </h4>
+<p> Modify a user here<p>
+    
     <form action="modifyUser.php?uid=<?php echo uniqid()?>" role="form" method="get">
         <div class="form-group">
             <h3>Search the user to modify by giving the username</h3>
@@ -80,35 +84,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET"){
         <button class="btn btn-default" type="submit">Find the user</button>
 
     </form>
+    '; } ?>
+    <?php 
+if(!empty($username) && isset($id)) {
+echo "Modifiy the user <b>".$username."</b></br>";
+echo '
     <form action="modifyUser.php" role="form" method="post">
-        <h3> Change this user parameters </h3>
+        <p> Change this user parameters <p>
         <div class="form-group">
+        <label for="TFusername">Username: </label>
+            <input id="TFusername" name="TFusername" class="form-control" type="text" placeholder="'.$username.'">
             <label for="TFpassword">Password: </label>
-            <?php  echo $password ?>
-            <input id="TFpassword" name="TFpassword" class="form-control" type="text" placeholder="PASSWORD">
+            <input id="TFpassword" name="TFpassword" class="form-control" type="text" placeholder="'.$password.'">
         </div>
 
 
         <div class="form-group">Choose the activity:
             <input type="radio" name="TFvalidity"
-                <?php if (isset($validity) && $validity=="1") echo "checked";?>
+               
                    value="1">Active
             <input type="radio" name="TFvalidity"
-                <?php if (isset($validity) && $validity=="0") echo "checked";?>
+                
                    value="0">Inactive
         </div>
 
         <div class="form-group">Choose the role:
             <input type="radio" name="TFrole"
-                <?php if (isset($role) && $role=="collaborateur") echo "checked";?>
+                
                    value="collaborateur">Collaborateur
             <input type="radio" name="TFrole"
-                <?php if (isset($role) && $role=="administrateur") echo "checked";?>
+                
                    value="administrateur">Administrateur
         </div>
 
         <button class="btn btn-warning" type="submit">Save changes</button>
     </form>
+'; } else if( !isset($id) && !empty($username)) {
+echo "this user doesn't exist"; } 
+?>
 </div>
 </body>
 <?php //include 'fragments/footer.php';?>
